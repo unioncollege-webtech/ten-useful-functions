29e2b6c68ebe13e42aabbd91c61d8f3bf6292f89
mrdcs92 <mrdcs92@gmail.com>
2015-10-15 21:28:11 +0000

    implemented exponential function

---
+++ ten_useful_functions.js
@@ -0,0 +1,30 @@
+// JavaScript File
+// Separate files for each function, or all together
+
+function DecimalToBinary() {
+    
+}
+
+function DecimalToHexadecimal() {
+    
+}
+
+function exponential(number, exponent) {
+    var initialNum = number;
+    for (var i = 1; i < exponent; i++) {
+        number = number * initialNum;
+    }
+    return number;
+}
+
+function primeNumbers(number) {
+    //check if number has a remainder
+    if ((number % anyNumber) !== 0) return true;
+    //check if number is non divisible
+}
+
+function factorization() {
+    
+}
+
+console.log(exponential(3, 5));
