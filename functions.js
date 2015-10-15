71be3a2a50e6fd1628e5c95fbbb1fd4bd7ed1737
moser2010 <cmoser351@gmail.co>
2015-10-15 21:27:54 +0000

    first commit

---
+++ functions.js
@@ -0,0 +1,36 @@
+
+
+function angle (aNum) {
+       if ( aNum === 90) {
+        console.log("It's a right Angle");
+    } else if (aNum <= 90 && aNum > 0) {
+        console.log("It's an acute Angle");
+    } else if (aNum == 180) {
+        console.log("It's a straight Angle");
+    } else if (aNum < 180 && aNum > 90) {
+        console.log("It's a obtuse Angle");
+    } else if (aNum >180 && aNum < 360) {
+        console.log("It's a reflex Angle");
+    } else if (aNum == 360) {
+        console.log("It's a Circle ");
+    } else {
+        console.log("this is not a valid number");
+    }
+}
+
+
+var aNum = prompt("Please enter an angle degree", "");
+if (aNum != null) {
+    document.getElementById("demo").innerHTML =
+    "Hello " + aNum + "! How are you today?";
+}
+
+angle(25);
+angle(60);
+angle(80);
+angle(90);
+angle(150);
+angle(180);
+angle(250);
+angle(360);
+angle(400);
