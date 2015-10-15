f83595096b71ed0d85b72f72774a9627ad620228
Matilda Kond <tmiacadet@gmail.com>
2015-10-15 21:50:52 +0000

    ten-useful function

---
+++ ten-useful-functions.js
@@ -0,0 +1,16 @@
+
+//function 1
+var total_count = 0, count = 1;
+while (count <= 10)
+{
+  total_count += count;
+  count += 1;
+}
+console.log(total_count);
+
+// function 2
+function sum(array)
+{
+    return array(function(a, b)
+    (return a + b; }, 0);
+}
