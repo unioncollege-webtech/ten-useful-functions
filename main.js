45157c7d7dd2f758024367dfa540249226904fa8
crodeheaver <colbyrodeheaver@hotmail.com>
2015-10-18 05:16:22 +0000

    progress on camelcase

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ var lessthan = require("./lessthan");
 var largest = require("./largest");
 var sentenceCase = require("./sentenceCase");
 var snake_case = require("./snakecase");
+var camelcase = require("./camelcase");
 
  var input = "The moon is our natural satellite. it rotates around the Earth!";
- 
+ var bermuda = "Bermuda Love Triangle is Awesome!!";
 
-console.log(snake_case(input));
+console.log(camelcase(input));
+console.log(camelcase(bermuda));
 
 
--- snakecase.js
+++ snakecase.js
@@ -1,4 +1,5 @@
 "use strict";
+
 module.exports = function(string){
     var output = "";
     var words = string.match(/\b[^\s]+\b/gi);
