3eb0c8bbd46f56fd4a7c966edf171865a27219d7
crodeheaver <colbyrodeheaver@hotmail.com>
2015-10-19 00:30:44 +0000

    a few more functions

--- kebobcase.js
+++ kebobcase.js
@@ -3,6 +3,6 @@
 module.exports = function(string){
     var output = string.match(/[a-z]+|[A-Z]+[a-z]*/g).map(function(value){
         return value.toLowerCase();
-    });
-    return output.join("_");
+    });;
+    return output.join("-");
 }
--- main.js
+++ main.js
@@ -2,10 +2,13 @@
 var range = require("./range");
 var greaterthan = require("./greaterthan");
 var lessthan = require("./lessthan");
+var greaterthanequalto = require("./greaterthanequalto");
+var lessthanequalto = require("./lessthanequalto");
 var largest = require("./largest");
 var sentenceCase = require("./sentenceCase");
 var snake_case = require("./snakecase");
 var camelcase = require("./camelcase");
+var kebobcase = require("./kebobcase");
 
  var input = "The moon is our natural satellite. it rotates around the Earth!";
  var bermuda = "Bermuda Love Triangle is Awesome!!";
@@ -13,4 +16,8 @@ var camelcase = require("./camelcase");
 console.log(camelcase(input));
 console.log(camelcase(bermuda));
 
+console.log(snake_case(camelcase(input)));
+console.log(snake_case(camelcase(bermuda)));
 
+console.log(kebobcase(camelcase(input)));
+console.log(kebobcase(camelcase(bermuda)));
