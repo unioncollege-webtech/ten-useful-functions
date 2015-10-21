2203e0ba8b8edee13b03c5bc2c9de697be0b3a66
crodeheaver <colbyrodeheaver@hotmail.com>
2015-10-21 15:10:26 +0000

    basic calls done

--- main.js
+++ main.js
@@ -9,3 +9,25 @@ var sentenceCase = require("./sentenceCase");
 var snake_case = require("./snakecase");
 var camelcase = require("./camelcase");
 var kebobcase = require("./kebobcase");
+
+var numRange = range(4,100,6);
+
+var greaterthanFive = greaterthan(5);
+
+var lessthanSix = lessthan(6);
+
+var greaterthanTen = greaterthanequalto(10);
+
+var lessthanequaltoTen =  lessthanequalto(10);
+
+var bigger = largest(17,2);
+
+var base = "A walk to the moon and back.";
+
+var sentence = sentenceCase(base);
+
+var camel = camelcase(base);
+
+var snake = snake_case(camel);
+
+var kebob = kebobcase(camel);
