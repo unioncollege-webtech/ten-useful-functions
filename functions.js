04045262a6b6ab02c88f056596ca4fe674e4a67c
greenitthe <greenitthe@gmail.com>
2016-10-03 18:41:12 -0500

    Completed

--- functions.js
+++ functions.js
@@ -29,3 +29,45 @@ function isOdd(num) {
   }
 }
 console.log("Is Odd: 8 -> " + isEven(8));
+
+function largest(a,b) {
+  return (a>b) ? true : false;
+}
+console.log("Largest: 7>6 -> " + largest(7,6));
+
+function smallest(a,b) {
+  return (a<b) ? true : false;
+}
+console.log("Largest: 7<6 -> " + largest(7,6));
+
+function abrahamLincoln(string) {
+  if (upperCase(string) === 'ABRAHAM LINCOLN') {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log("Abraham lincoln: abrAhAm LINcoLN -> " + abrahamLincoln('abrAhAm LINcoLN'));
+
+function capitalize(string) {
+  return upperCase(string.substring(0, 1)).concat(string.substring(1));
+}
+console.log("Capitalize: capitalize me captain! -> " + capitalize('capitalize me captain!'));
+
+function seperate(string, seperator) {
+  var out = "";
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] === seperator) {
+      out += " ";
+    } else {
+      out += string[i];
+    }
+  }
+  return out;
+}
+console.log("Seperate: james;is;casual ';' -> " + seperate('james;is;casual', ';'));
+
+function replaceString(string, findStr, replaceStr) {
+  return string.replace(findStr, replaceStr);
+}
+console.log("Replace String: Joseph is funny and a wonderful man 'Joseph' 'James' -> " + replaceString('Joseph is funny and a wonderful man', 'Joseph', 'James'));
